b51577942b8ff722eda82b51b9ccea39d9066ebd
metleeha <nodongth@gmail.com>
2020-12-17 23:00:47 +0900

    Add DevOps: Jira CICD

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,7 +1,13 @@
-const Javascript = './Javascript'
+const Javascript = './Javascript';
+const DevOps = './DevOps';
+
 const JavascriptList = [
     'regular-expression-1',
     'regular-expression-2'
+]
+
+const DevOpsList = [
+    'jira-cicd'
 ]
 
 module.exports = [
@@ -9,4 +15,8 @@ module.exports = [
         title: 'Javascript',
         children: JavascriptList.map(v => `${Javascript}/${v}`)
     },
+    {
+        title: 'DevOps',
+        children: DevOpsList.map(v => `${DevOps}/${v}`)
+    }
 ]
